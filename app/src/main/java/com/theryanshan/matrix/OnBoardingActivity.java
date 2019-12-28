99b6d0488d6edaf272a3216c90e1ed19a5c1b4db
Ryan Shan <ryanshan28@gmail.com>
2019-12-28 12:08:34 -0800

    Created an OnBoardingPageAdapter, which defines how fragment will be displayed in the viewpager

--- app/src/main/java/com/theryanshan/matrix/OnBoardingActivity.java
+++ app/src/main/java/com/theryanshan/matrix/OnBoardingActivity.java
@@ -1,14 +1,27 @@
 package com.theryanshan.matrix;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class OnBoardingActivity extends AppCompatActivity {
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+
+        viewPager = findViewById(R.id.viewpager);
+        OnBoardingPageAdapter onBoardingPageAdapter = new OnBoardingPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(onBoardingPageAdapter);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.colorAccent));
     }
 }
---
+++ app/src/main/java/com/theryanshan/matrix/OnBoardingPageAdapter.java
@@ -0,0 +1,36 @@
+package com.theryanshan.matrix;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class OnBoardingPageAdapter extends androidx.fragment.app.FragmentPagerAdapter {
+    private static int NUM_ITEMS = 1;
+
+    public OnBoardingPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return LoginFragment.newInstance();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position) {
+            case 0:
+                return "Login";
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+}
